23773cbd28e95a568d41912980879d10b62ca604
ValiakhmetovBulat <valiaxmetovb134@gmail.com>
2022-01-31 17:13:46 +0300

    Добавьте файлы проекта.

---
+++ TestEx2/Weather.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TestEx2
+{
+    public class Temperature
+    {
+        public double temp;
+        public double humidity;
+    }
+    public class Sun
+    {
+        public double sunrise;
+        public double sunset;
+    }
+    public class WeatherResponce
+    {
+        public Temperature main;
+        public Sun sys;
+        public string name;
+        public double timezone;
+    }
+}
